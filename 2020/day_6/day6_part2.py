6dd95f63bea9afb6784c36d57ad68a9c7b5f7f1b
kyle <kforker1@gmail.com>
2022-06-25 17:45:11 -0400

    Add files via upload

---
+++ 2020/day_6/day6_part2.py
@@ -0,0 +1,43 @@
+
+linesList = []
+with open("input.txt", "r") as infile:
+    for line in infile:
+        linesList.append(list(line.split()))
+
+tempMatrix = []
+mainMatrix = []
+count = True
+for list in linesList:
+    for i in list:
+        tempMatrix.append(i)
+    if list == []:
+        count = False
+    if count == False: 
+        mainMatrix.append(tempMatrix)
+        tempMatrix = []
+        count = True
+mainMatrix.append(tempMatrix)
+
+sum = 0
+for list in mainMatrix:
+    questionsDict = {}
+    listLen = len(list)
+
+    for sublist in list:
+        if len(sublist) > 1:
+            for char in sublist:
+                if char in questionsDict:
+                    questionsDict[char] += 1
+                else:
+                    questionsDict[char] = 1 
+        else:
+            if sublist in questionsDict:
+                questionsDict[sublist] += 1
+            else:
+                questionsDict[sublist] = 1
+    for q in questionsDict.values():
+        if q == listLen:
+            sum += 1
+    #print(questionsDict.values(), listLen)
+
+print(sum)

e26b3e5714368034cc3b8ef7474e7a84b42964ef
kyle <kforker1@gmail.com>
2023-12-02 02:11:33 -0500

    Add files via upload

---
+++ 2023/day2/part2.py
@@ -0,0 +1,50 @@
+
+class Game():
+    def __init__(self, gameid, sets):
+        self.gameid = gameid
+        self.sets = sets
+
+
+def get_num_color(s):
+    try:
+        return int(s[0:2])
+    except:
+        return int(s[0])
+
+
+# 12 red; 13 green; 14 blue
+def main():
+    with open('input.txt', 'r') as input_file:
+        data = [line.rstrip() for line in input_file]
+    
+    g_list = []
+    for d in data:
+        temp = d.split(':')
+        temp[0] = temp[0][5:]
+        set_data_temp = temp[1].strip().split('; ')
+        for i,sd in enumerate(set_data_temp):
+            set_data_temp[i] = sd.split(', ')
+        g_list.append(Game(int(temp[0]), set_data_temp))
+    
+    powers = []
+    for g in g_list:
+        max_b, max_g, max_r = 0, 0, 0
+        for set in g.sets:
+            for s in set:
+                nc = get_num_color(s)
+                if 'blue' in s:
+                    if nc > max_b:
+                        max_b = nc
+                elif 'green' in s:
+                    if nc > max_g:
+                        max_g = nc
+                else:
+                    if nc > max_r:
+                        max_r = nc
+        powers.append(max_b * max_g * max_r)
+    print(sum(powers))
+
+
+
+if __name__ == '__main__':
+    main()

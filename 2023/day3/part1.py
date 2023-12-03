56cfed033922a05363712eb4456807630ebd79b0
kyle <kforker1@gmail.com>
2023-12-03 14:39:23 -0500

    Add files via upload

---
+++ 2023/day3/part1.py
@@ -0,0 +1,85 @@
+import numpy as np
+import re
+
+def main():
+    with open('input.txt', 'r') as in_file:
+        data = [[l.rstrip()] for l in in_file]
+    fd = []
+    for d in data:
+        t = []
+        for x in d[0]:
+            t.append(x)
+        fd.append(t)
+    da = np.array(fd)
+    ignore = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+    num_check = re.compile(r"[0-9]")
+    nums = []
+    checked = []
+    for row in range(0, len(da) - 1):
+        for col in range(0, len(da[0]) - 1):
+            # (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1) 
+            for y_movement, x_movement  in ((-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1), (0, -1)):
+                temp_row = row + x_movement
+                temp_col = col + y_movement
+                c = 1
+                tn_flag = False
+                if (temp_row == row and temp_col == col) or (temp_row < 0 or temp_col < 0):
+                    continue
+                elif not da[(col, row)] in ignore:
+                    if re.search(num_check, str(da[(temp_col, temp_row)])):
+                        t_num = str(da[(temp_col, temp_row)])
+                        
+                        while True:
+                            try:
+                                if re.search(num_check, str(da[(temp_col, temp_row + c)])):
+                                    if (temp_col, temp_row + c) in checked:
+                                        # print('p-found', (temp_col, temp_row + c))
+                                        break
+                                    t_num += str(da[(temp_col, temp_row + c)])
+                                    checked.append((temp_col, temp_row))
+                                    checked.append((temp_col, temp_row + c))
+                                    # print('p', c, [temp_col, temp_row + c], t_num)
+                                    c += 1
+                                    tn_flag = True
+                                else: 
+                                    break
+                            except:
+                                break
+                        c = 1
+                        while True:
+                            try:
+                                if re.search(num_check, str(da[(temp_col, temp_row - c)])):
+                                    if c < 0 or (temp_col, temp_row - c) in checked:
+                                        # print('n-found', (temp_col, temp_row - c))
+                                        break
+                                    t_num = str(da[(temp_col, temp_row - c)]) + t_num
+                                    checked.append((temp_col, temp_row))
+                                    checked.append((temp_col, temp_row - c))
+                                    # print('n', c, [temp_col, temp_row - c], t_num)
+                                    c += 1
+                                    tn_flag = True
+                                else: 
+                                    break
+                            except:
+                                break
+                        # print(checked)
+                        
+                        if tn_flag:
+                            # print(t_num)
+                            nums.append(t_num)
+                        else:
+                            if not (temp_col, temp_row) in checked:
+                                nums.append(str(da[(temp_col, temp_row)]))
+                        #print([col, row], [temp_col, temp_row], da[(temp_col, temp_row)])
+
+    print(nums, sum([int(i) for i in nums]))
+                    
+                        
+                    
+
+
+if __name__ == '__main__':
+    main()
+
+# checked
+# 531492 <- too low

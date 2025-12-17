d60ffa1bab108b33134a5dfb2288c79a507687d2
kyle <kforker1@gmail.com>
2025-12-16 21:06:53 -0500

    Updated gitignore to ignore python test scripts

--- 2024/day7/part1.py
+++ 2024/day7/part1.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from itertools import product
 
-with open('input.txt', 'r') as infile:
+with open('test_input.txt', 'r') as infile:
     data = [i.rstrip() for i in infile.readlines()]
 
 input_dict = defaultdict(list)
@@ -11,31 +11,33 @@ for d in data:
 
 def eval_equation(equation):
     curr_number = ''
-    curr_op = ''
-    result = 0
-    for c, e in enumerate(equation):
-        if e.isnumeric():
-            curr_number += e
-        else:
-            # print('OP', curr_op, result, curr_number)
-            if result == 0:
-                result = int(curr_number)
+    ops = []
+    for char in equation:
+        if char.isdigit():
+            curr_number += char
+        elif char in '+*':
+            if curr_number:
+                ops.append((int(curr_number)))
                 curr_number = ''
-                curr_op = e
-            else:
-                if curr_op == '+':
-                    result += int(curr_number)
-                elif curr_op == '*':
-                    result *= int(curr_number) 
-                curr_number = ''
-                curr_op = e
-        if c == len(equation) - 1:
-            if curr_op == '+':
-                result += int(curr_number)
-            elif curr_op == '*':
-                result *= int(curr_number)
-    return result
+            ops.append(char)
+
+    if curr_number:
+        ops.append(int(curr_number))
+
+    result = ops[0]
+    i = 1
+    while i < len(ops):
+        curr_op = ops[i]
+        opd = ops[i + 1]
+        if curr_op == '+':
+            result += opd
+        elif curr_op == '*':
+            result *= opd
+
+        i += 2
+
     # print(result)
+    return result
 
 result = 0
 for i in input_dict:
@@ -44,10 +46,14 @@ for i in input_dict:
     for op in ops_list:
         equation = ''.join(f"{input_dict[i][j]}{op[j]}" for j in range(num_pos)) + str(input_dict[i][-1])
         equation_result = eval_equation(equation)
+        print(equation_result, i, equation)
         if equation_result == i:
             # print(equation, equation_result)
             result += i
-            break
+            #break
 
 print(result)
 # print(input_dict)
+
+
+# 3245122494055 > too low
